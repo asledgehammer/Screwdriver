d964d2a1a222a90f9b01fc61470f352197e73c0e
DeltaMango <DeltaMango@github.com>
2022-07-13 03:52:59 -0500

    Added Java packaged primitive pseudo-types.

--- src/ts/screwdriver/util/JavaTypes.ts
+++ src/ts/screwdriver/util/JavaTypes.ts
@@ -5,3 +5,11 @@ export type int = number;
 export type float = number;
 export type double = number;
 export type long = number;
+
+export type Boolean = boolean | null;
+export type Byte = number | null;
+export type Short = number | null;
+export type Integer = number | null;
+export type Float = number | null;
+export type Double = number | null;
+export type Long = number | null;
